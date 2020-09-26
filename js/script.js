414d538b741f3f346ad6a7837ab08859c1f02a60
28torigeorge <txtdrive@gmail.com>
2020-09-26 22:06:41 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ $('#fullpage').fullpage({
+ 	navigation:true,
+ 	navigationPosition: 'right',
+ 	scrollingSpeed:2700,
+ 	anchors: ['top', '1st', '2nd', 'last'],
+ });
+});
